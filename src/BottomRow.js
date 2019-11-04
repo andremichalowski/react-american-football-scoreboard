436c519f15290fcefd8431993c23e446344bced5
amichalo421 <andre.michalowski@gmail.com>
2019-11-04 16:32:36 +0100

    Stretch_Downs_Attempted

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -5,48 +5,48 @@ import "./App.css";
 // const [quarterCount, setQuarterCount] = useState(0); (ERROR)
 
 const BottomRow = () => {
-  // const [down, setDown] = useState(1);
+  // // const [down, setDown] = useState(1);
 
-  // const nextDown = () => {
-  //   setDown(down + 1);
-  // }
+  // // const nextDown = () => {
+  // //   setDown(down + 1);
+  // // }
 
-  // const [toGo, setToGo] = useState(50);
+  // // const [toGo, setToGo] = useState(50);
 
-  // const decreaseToGo = () => {
-  //   setToGo(toGo - 1);
-  // }
+  // // const decreaseToGo = () => {
+  // //   setToGo(toGo - 1);
+  // // }
 
-  // const [ballOn, setBallOn] = useState(50);
+  // // const [ballOn, setBallOn] = useState(50);
 
-  // const decreaseBallOn = () => {
-  //   setBallOn(ballOn - 1);
-  // }
-  return (
-    <div className="bottomRow">
-      <div className="down">
-        <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
-        {/* <div className="down__value">{down}</div>
-        <button onClick={nextDown} className="button">Next Down</button> */}
-      </div>
-      <div className="toGo">
-        <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
-        {/* <div className="toGo__value">{toGo}</div>
-        <button onClick={decreaseToGo} className="button">Decrease 1 Yard</button> */}
-      </div>
-      <div className="ballOn">
-        <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
-        {/* <div className="ballOn__value">{ballOn}</div>
-        <button onClick={decreaseBallOn} className="button">Decrease 1 Yard</button> */}
-      </div>
-      <div className="quarter">
-        <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
-        {/* <div className="quarter__value">{quarterCount - setQuarterCount}</div> */}
-      </div>
+  // // const decreaseBallOn = () => {
+  // //   setBallOn(ballOn - 1);
+  // // }
+  // return (
+  //   <div className="bottomRow">
+  //     <div className="down">
+  //       <h3 className="down__title">Down</h3>
+  //       <div className="down__value">3</div>
+  //       {/* <div className="down__value">{down}</div>
+  //       <button onClick={nextDown} className="button">Next Down</button> */}
+  //     </div>
+  //     <div className="toGo">
+  //       <h3 className="toGo__title">To Go</h3>
+  //       <div className="toGo__value">7</div>
+  //       {/* <div className="toGo__value">{toGo}</div>
+  //       <button onClick={decreaseToGo} className="button">Decrease 1 Yard</button> */}
+  //     </div>
+  //     <div className="ballOn">
+  //       <h3 className="ballOn__title">Ball on</h3>
+  //       <div className="ballOn__value">21</div>
+  //       {/* <div className="ballOn__value">{ballOn}</div>
+  //       <button onClick={decreaseBallOn} className="button">Decrease 1 Yard</button> */}
+  //     </div>
+  //     <div className="quarter">
+  //       <h3 className="quarter__title">Quarter</h3>
+  //       <div className="quarter__value">4</div>
+  //       {/* <div className="quarter__value">{quarterCount - setQuarterCount}</div> */}
+  //     </div>
     </div>
   );
 };
