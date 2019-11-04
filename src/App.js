969a7ffefa029863c01887279de34512af532d8d
amichalo421 <andre.michalowski@gmail.com>
2019-11-04 14:48:38 +0100

    Step-3_Completed

--- src/App.js
+++ src/App.js
@@ -19,21 +19,22 @@ function App() {
   const [awayCount, setAwayCount] = useState(0);
   const [ awayFieldGoal, setAwayFieldGoal] = useState(0);
 
+  /* TODO STEP 3 - We need to change the hardcoded values in 
+            these divs to accept dynamic values from our state. */
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
+          {/* //HOME// */}
           <div className="home">
             <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">32</div>
+            <div className="home__score">{count + homeFieldGoal}</div>
           </div>
           <div className="timer">00:03</div>
+          {/* //AWAY// */}
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{count + awayFieldGoal}</div>
           </div>
         </div>
         <BottomRow />
